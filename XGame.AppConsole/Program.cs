e580bfa8b89782e40ec0781a8e3961e49ab144c6
Wesley Machado <weslley182@gmail.com>
2021-02-04 10:14:01 -0300

    Habilitando Migrations

---
+++ XGame.AppConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XGame.AppConsole
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
